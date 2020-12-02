e4bc0243f0bd6f7fc694e1eaa394e3eab76f4033
Yangeng Chen <yangengchen@gmail.com>
2020-12-02 01:15:17 -0500

    Added input.txt

--- AdventofCode/src/Day1.java
+++ AdventofCode/src/Day1.java
@@ -5,7 +5,7 @@ import java.util.*;
 public class Day1 {
 
 	public static void main(String[] args) {
-		String fileName = "/Users/yangengchen/Desktop/input.txt";
+		String fileName = "./input.txt";
 		List<Integer> list = readFromFileAndPopulateList(fileName);
 		puzzle1(list);
 		puzzle2(list);
